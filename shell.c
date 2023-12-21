cd5d952b11b08c2f7b5bd077bc4f933f3e958533
sebteouii <sebteouiikhoubayb@gmail.com>
2023-12-21 13:50:25 +0100

    add idx

--- shell.c
+++ shell.c
@@ -6,24 +6,25 @@
  */
 int main(int ac, char **argv)
 {
-    char *line = NULL, **command = NULL;
-    int status = 0;
-    (void)ac;
-    while (1)
-    {
-        line = read_line();
-        if (line == NULL)
-        {
-            if (isatty(STDIN_FILENO))
-            {
-                write(STDOUT_FILENO, "\n", 1);
-            }
-            return (status);
-        }
-        command = tokenizer(line);
-        if (command == NULL)
-            continue;
+	char *line = NULL, **command = NULL;
+	int status = 0, idx = 0;
+	(void)ac;
+	while (1)
+	{
+		line = read_line();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+			{
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			return (status);
+		}
+		idx++;
+		command = tokenizer(line);
+		if (command == NULL)
+			continue;
 
-        status = _execute(command, argv);
-    }
+		status = exec(command, av, idx);
+	}
 }
